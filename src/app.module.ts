3bcb8ba03e17ed0a7a47fc3c156306abe02e7604
anxium <axel.avx@gmail.com>
2020-11-13 11:32:53 +0100

    Add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmCongif } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(typeOrmCongif), TasksModule],
 })
 export class AppModule {}
